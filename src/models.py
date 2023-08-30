190616b201ddcd4f18ffc142fa8e2f5b2ba30fb0
wpeguero <wpeguerorosario@outlook.com>
2023-08-29 20:23:35 -0400

    MINOR CHANGE: Fine Tuning Model Parameters
    Started fine tuning parameters to make the text smoother in terms of
    sentence structer. Managed to develop a cohesive model that is able to
    develop actual sentences. The only issue is the problem with relevance
    in relation to the previous text. Most stories created appear to have
    random sentences placed together.

--- src/models.py
+++ src/models.py
@@ -10,23 +10,23 @@ generative models.
 from tensorflow.keras.layers import Dense, LSTM, Input, Embedding, Dropout
 from tensorflow.keras.models import Model
 from tensorflow.keras.losses import SparseCategoricalCrossentropy
-from tensorflow.keras.optimizers import RMSprop
+from tensorflow.keras.optimizers import RMSprop, Adafactor
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.utils import plot_model
 import numpy as np
 import random
 from pipeline import clean_text, generate_sequences
 
-EPOCHS = 1000
+EPOCHS = 1_000
 BATCH= 32
-version=2
+version=18
 
 def _main():
     filename = "./data/aesop_tales.txt"
     with open(filename, encoding='utf-8') as f:
         text = f.read()
     seq_length = 20
-    start_story = "|" * seq_length
+    start_story = "| " * seq_length
     start = text.find("THE FOX AND THE GRAPES\n\n\n")
     end = text.find("ILLUSTRATIONS\n\n\n[")
     text = text[start:end]
@@ -36,29 +36,20 @@ def _main():
     tokenizer.fit_on_texts([text])
     total_words = len(tokenizer.word_index) + 1
     token_list = tokenizer.texts_to_sequences([text])[0]
+    print(total_words)
     # Parameters
-    n_units = 256
-    embedding_size = 100
+    n_units = 512
+    embedding_size = 200
     # build Dataset
     X, y, num_seq = generate_sequences(token_list,sequence_length=seq_length, step=1)
-    print(X.shape)
-    print(y.shape)
     # Build Model
-    text_in = Input(shape=(None,))
-    x = Embedding(total_words, embedding_size)(text_in)
-    x = LSTM(n_units)(x)
-    x = Dropout(0.2)(x)
-    text_out = Dense(total_words, activation='softmax')(x)
-    model = Model(text_in, text_out)
-    #model = TextGenerator(total_words, embedding_size, n_units)
-    model.compile(optimizer=RMSprop(learning_rate=0.001), loss='categorical_crossentropy')
-    #model.build(input_shape=(None,))
+    model = TextGenerator(total_words, embedding_size, n_units)
+    model.compile(optimizer='adam', loss='categorical_crossentropy')
     plot_model(model, to_file='models/diagrams/model_v{}.png'.format(version), show_shapes=True, show_dtype=True)
     model.fit(X, y, epochs=EPOCHS, batch_size=BATCH, shuffle=True)
     model.save('models/model_v{}'.format(version))
 
-
-class TextGenerator(Model):
+def TextGenerator(total_words:int, embedding_size:int, n_units:int):
     """
 
     Model For Designing a Text Generator.
@@ -70,42 +61,14 @@ class TextGenerator(Model):
     developing interesting story-tellers based on books
     written by famous authors.
     """
-
-    def __init__(self, vocab_size, embedding_dim, rnn_units):
-        """
-
-        Initialize the class.
-
-        ...
-
-        Parameters
-        ----------
-        vocab_size : Integer
-            The number of unique tokens found within the
-            corpus.
-
-        embedding_dim : Integer
-            The number of dimensions within the embedding layer.
-
-        rnn_units : Integer
-            The number of separate LSTM units within the LSTM layer.
-        """
-        super(TextGenerator, self).__init__()
-        self.embedding = Embedding(vocab_size, embedding_dim)
-        self.lstm1 = LSTM(rnn_units)
-        self.lstm2 = LSTM(int(rnn_units/2))
-        self.dropout = Dropout(0.2)
-        self.dense = Dense(vocab_size, activation="softmax")
-
-    def call(self, inputs):
-        """Call upon the layers of the custom model for training."""
-        x = inputs
-        x = self.embedding(x)
-        x = self.lstm1(x)
-        x = self.lstm2(x)
-        x = self.dropout(x)
-        outputs = self.dense(x)
-        return outputs
+    text_in = Input(shape=(None,))
+    x = Embedding(total_words, embedding_size)(text_in)
+    x = LSTM(n_units, return_sequences=True)(x)
+    x = LSTM(int(n_units * 2))(x)
+    x = Dropout(0.3)(x)
+    text_out = Dense(total_words, activation='softmax')(x)
+    model = Model(text_in, text_out)
+    return model
 
 
 if __name__ == "__main__":
